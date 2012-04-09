b6a7e6c88f0f03a80475534c8d5e64c3fdc5f269
Mykhailo Kotsur <miccots@gmail.com>
2012-04-09 14:07:45 +0200

    Improved e2e coverage moved json mocks to separate files

--- js/spec/e2e/repositoryLinking.spec.js
+++ js/spec/e2e/repositoryLinking.spec.js
@@ -7,25 +7,34 @@ describe("Repository linking case of app", function() {
             'div[ng-controller="RepoLinkageController"]',
             "Repo linkage controler"
         ).element('input[type="button"]');
+
     });
 
-    it("should open the page", function() {
+
+    it("should take repo URL from hash", function() {
+        browser().navigateTo("/index.html#https://api.github.com/repos/e2e/test");
+        expect(binding('repo.url')).toEqual('https://api.github.com/repos/e2e/test');
+        expect(binding('repo.apiUrl')).toEqual('https://api.github.com/repos/e2e/test');
+
+        expect(repoLinkageButton.attr('disabled')).toBeFalsy();
+    });
+
+    it("should open the page and download repo information", function() {
         browser().navigateTo("/index.html");
 
         expect(repoLinkageButton.attr('disabled')).toBeTruthy();
 
-        input("repo.url").enter("https://github.com/angular/angular.js");
-        expect(binding('repo.url')).toEqual('https://github.com/angular/angular.js');
-        expect(binding('repo.apiUrl')).toEqual('https://api.github.com/repos/angular/angular.js');
+        input("repo.url").enter("https://github.com/e2e/test");
+        expect(binding('repo.url')).toEqual('https://github.com/e2e/test');
+        expect(binding('repo.apiUrl')).toEqual('https://api.github.com/repos/e2e/test');
 
         expect(repoLinkageButton.attr('disabled')).toBeFalsy();
-    })
 
-    it("should take repo URL from hash", function() {
-        browser().navigateTo("#https://api.github.com/repos/mkotsur/angular.js");
-        expect(binding('repo.url')).toEqual('https://github.com/angular/angular.js');
-        expect(binding('repo.apiUrl')).toEqual('https://api.github.com/repos/angular/angular.js');
+        repoLinkageButton.click();
 
-        expect(repoLinkageButton.attr('disabled')).toBeFalsy();
+        expect(repeater(".commits ul", "List of commits").count()).toEqual(2);
+
+        expect(using('.commits ul', "List of commits").element("a:first").text()).toEqual("15c1fe392942b70e456f10afbdfd9c3329249a43");
     });
+
 });
